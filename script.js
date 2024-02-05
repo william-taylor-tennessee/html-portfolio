03ba06a2a733de328ef0a88603f2ec94f133b4e5
william-taylor-tennessee <67650399+william-taylor-tennessee@users.noreply.github.com>
2024-02-05 09:53:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 "use strict";
 
 let selectedValue = document.getElementById("band").value;
+let bandImage = "";
+let bandName = "";
+let bandInfo = "";
 
 function checkBand(selectedValue) {
   switch (selectedValue) {
     case "Beatles":
-      beatlesFunction();
+      bandImage = "Beatles_early days.jpg";
+      bandName = "Beatles";
+      bandInfo =
+        "Beatles - This info needs to be developed for this band. I will place some interesting info later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later.";
+      bandFunction();
       break;
 
     case "Billy Idol":
-      billyIdolFunction();
+      bandImage = "Billy Idol.jpg";
+      bandName = "Billy Idol";
+      bandInfo =
+        "Billy Idol - This info needs to be developed for this band. I will place some interesting info later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later.";
+      bandFunction();
       break;
 
     case "Rolling Stones":
-      rollingStonesFunction();
+      bandImage = "Rolling Stones.webp";
+      bandName = "Rolling Stones";
+      bandInfo =
+        "Rolling Stones - This info needs to be developed for this band. I will place some interesting info later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later.";
+      bandFunction();
       break;
 
     default:
@@ -22,19 +37,17 @@ function checkBand(selectedValue) {
   }
 }
 
-function beatlesFunction() {
+function bandFunction() {
   const a = document.createElement("div");
   const b = document.createElement("img");
   const c = document.createAttribute.src;
   const d = document.createElement("div");
   const e = document.createElement("H1");
-  const f = document.createTextNode("Beatles");
+  const f = document.createTextNode(bandName);
 
   const h = document.createElement("div");
   const i = document.createElement("p");
-  const beatlesInfo =
-    "This info needs to be developed for this band. I will place some interesting info later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later.";
-  const j = document.createTextNode(beatlesInfo);
+  const j = document.createTextNode(bandInfo);
 
   a.classList.add("wrapper");
   b.classList.add("band-image");
@@ -42,69 +55,11 @@ function beatlesFunction() {
   e.classList.add("band-name");
   h.classList.add("wrapper");
   i.classList.add("band-info");
-
-  document.body
-    .appendChild(a)
-    .appendChild(b)
-    .setAttribute("src", "Beatles_early days.jpg");
+  document.body.appendChild(a).appendChild(b).setAttribute("src", bandImage);
   document.body.appendChild(d).appendChild(e).appendChild(f);
   document.body.appendChild(h).appendChild(i).appendChild(j);
-}
 
-function billyIdolFunction() {
-  const a = document.createElement("div");
-  const b = document.createElement("img");
-  const c = document.createAttribute.src;
-  const d = document.createElement("div");
-  const e = document.createElement("H1");
-  const f = document.createTextNode("Billy Idol");
-
-  const h = document.createElement("div");
-  const i = document.createElement("p");
-  const billyIdolInfo =
-    "This info needs to be developed for this band. I will place some interesting info later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later.";
-  const j = document.createTextNode(billyIdolInfo);
-
-  a.classList.add("wrapper");
-  b.classList.add("band-image");
-  d.classList.add("wrapper");
-  e.classList.add("band-name");
-  h.classList.add("wrapper");
-  i.classList.add("band-info");
-
-  document.body
-    .appendChild(a)
-    .appendChild(b)
-    .setAttribute("src", "Billy Idol.jpg");
-  document.body.appendChild(d).appendChild(e).appendChild(f);
-  document.body.appendChild(h).appendChild(i).appendChild(j);
-}
-
-function rollingStonesFunction() {
-  const a = document.createElement("div");
-  const b = document.createElement("img");
-  const c = document.createAttribute.src;
-  const d = document.createElement("div");
-  const e = document.createElement("H1");
-  const f = document.createTextNode("Rolling Stones");
-
-  const h = document.createElement("div");
-  const i = document.createElement("p");
-  const rollingStonesInfo =
-    "This info needs to be developed for this band. I will place some interesting info later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later. This is just a filler for later.";
-  const j = document.createTextNode(rollingStonesInfo);
-
-  a.classList.add("wrapper");
-  b.classList.add("band-image");
-  d.classList.add("wrapper");
-  e.classList.add("band-name");
-  h.classList.add("wrapper");
-  i.classList.add("band-info");
-
-  document.body
-    .appendChild(a)
-    .appendChild(b)
-    .setAttribute("src", "Rolling Stones.webp");
-  document.body.appendChild(d).appendChild(e).appendChild(f);
-  document.body.appendChild(h).appendChild(i).appendChild(j);
+  console.log(`Band Image is ${bandImage}`);
+  console.log(`Band Name is ${bandName}`);
+  console.log(`Band Info is ${bandInfo}`);
 }
